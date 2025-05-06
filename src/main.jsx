05aef50eae74895024047320b6938f3a45a38702
nooralaakso <bhm543@myy.haaga-helia.fi>
2025-05-06 20:22:13 +0300

    fix index element in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -13,14 +13,15 @@ createRoot(document.getElementById('root')).render(
   <React.StrictMode> {/* HashRouter is used to manage pare URLs */}
     <HashRouter>
       <Routes>
-        
-        <Route path="/" element={<App />} errorElement={<Error />}> {/* Main route that shows the App layout */}
-         
+
+        <Route path="/" element={<App />}>
+
+          <Route index element={<CustomerList />} /> {/* When URL is #/, show CustomerList */}
           <Route path="customers" element={<CustomerList />} /> {/* When URL is #/customers, show CustomerList */}
           <Route path="trainings" element={<TrainingList />} /> {/* When URL is #/trainings, show TrainingList */}
           <Route path="calendar" element={<Calendar />} /> {/* When URL is #/calendar, show Calendar */}
           <Route path='charts' element={<TrainingChart />} /> {/* When URL is #/charts, show TrainingChart */}
-          
+
         </Route>
       </Routes>
     </HashRouter>
